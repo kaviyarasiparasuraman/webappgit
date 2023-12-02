42f8d1eb4413be34569d67856f47343bfd85f2be
kaviyarasi.p <kaviyarasi.p@saint-gobain.com>
2023-12-02 11:56:45 +0530

    commit azure webapp changes

--- webappgit/Controllers/WeatherForecastController.cs
+++ webappgit/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace webappgit.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "kavi", "Bracing", "hiii", "Cool", "hello", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
